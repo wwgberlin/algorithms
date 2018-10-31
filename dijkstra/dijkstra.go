19a2282eb3c6ff9549f2020a928068a9ae7b98c2
Ronna Steinberg <ronna.steinberg@daliaresearch.com>
2018-10-31 15:47:31 +0100

    removed dijkstra

--- dijkstra/dijkstra.go
+++ dijkstra/dijkstra.go
@@ -7,6 +7,7 @@ import (
 	"container/heap"
 	"math"
 	. "github.com/wwgberlin/algorithms/graph"
+	"strconv"
 )
 
 type PQueue []Edge
@@ -46,41 +47,66 @@ func main() {
 	g = SampleGraph()
 
 	fmt.Println(g)
-	fmt.Println(Dijkstra(g, 2, 7))
-	fmt.Println(Dijkstra(g, 1, 7))
-	fmt.Println(Dijkstra(g, 3, 4))
-	fmt.Println(Dijkstra(g, 4, 2))
-	fmt.Println(Dijkstra(g, 4, 3))
-	fmt.Println(Dijkstra(g, 5, 1))
-	fmt.Println(Dijkstra(g, 5, 7))
-	fmt.Println(Dijkstra(g, 5, 3))
-	fmt.Println(Dijkstra(g, 6, 3))
-	fmt.Println(Dijkstra(g, 6, 2))
+	cases := []struct {
+		s int
+		t int
+	}{
+		{2, 7},
+		{1, 7},
+		{3, 4},
+		{4, 2},
+		{4, 3},
+		{5, 1},
+		{5, 7},
+		{5, 3},
+		{6, 3},
+		{6, 2},
+		{7, 1},
+	}
+
+	for _, c := range cases {
+		fmt.Printf("Calculating shortest path from %d to %d:\n", c.s, c.t)
+		path, distance := Dijkstra(g, c.s, c.t)
+		if len(path) == 0 {
+			fmt.Println("No path found")
+			continue
+		}
+		var s string
+		for i, v := range path {
+			if i != 0 {
+				s += "->"
+			}
+			s += strconv.Itoa(v)
+		}
+		fmt.Println(s, fmt.Sprintf("(%d)", distance))
+	}
 }
 
 /*
 Dijkstra(g,s) //simplified
 	pq := make empty priority queue
 	dist : make empty map from vertices to distances (weights)
+
+	for every vertex v in graph
+		dist[v] := infinite (math.MaxInt32)
 	prev := make empty map from vertex to vertex
 
 	pq.push(s, 0) //the distance of s from the source is 0
 	dist[src] := 0 //initialize the correct distance for the vertex s
 
 	while pq is not empty
-			u := pq.pop
-			pq.pop();
-			for each v neighbor of u
-		   alt := dist[u] + weight
-				if (dist[v] > alt) then
-					dist[v] := alt
-			   prev[v]
-					pq.push(v, alt);
+		u := pq.pop
+		pq.pop();
+		for each v neighbor of u
+			alt := dist[u] + weight
+			if (dist[v] > alt) then
+				dist[v] := alt
+			   	prev[v] := u
+				pq.push(v, alt);
 
-
-	// output the shortest path to t:
-		out:= make empty slice
-		v,ok:= t, prev[t]
+		//almost done :) output the shortest path to vertex t
+		out := make empty slice
+		v,ok := t, prev[t]
 		while ok
 			out:=prepend(v, out)
 			v,ok:= prev[v]
@@ -88,42 +114,4 @@ Dijkstra(g,s) //simplified
 		return out, dist[t]
  */
 func Dijkstra(g Graph, s int, t int) ([]int, int) {
-	pq := PQueue(make([]Edge, 0))
-	heap.Push(&pq, Edge{s, 0})
-
-	dist := make(map[int]int)
-
-	for i := range g {
-		dist[i] = math.MaxInt32
-	}
-
-	prev := make(map[int]int)
-	dist[s] = 0
-
-	for len(pq) > 0 {
-		e := heap.Pop(&pq).(Edge)
-		u := e.V
-
-		for _, e := range g[e.V] {
-			v := e.V
-			d := e.D
-
-			if dist[v] > dist[u]+d {
-				dist[v] = dist[u] + d
-				prev[v] = u
-				heap.Push(&pq, Edge{v, dist[v],})
-			}
-		}
-	}
-
-	var out []int
-
-	for v, ok := prev[t]; ok; v, ok = prev[v] {
-		out = append([]int{v}, out...)
-		if v == s {
-			break
-		}
-	}
-
-	return out, dist[t]
 }
